8482b8e9a45c3d245110c17120cfb6e05259360e
Abhisharma08 <aksharma223@gmail.com>
2026-03-23 18:38:42 +0530

    content updated

--- src/lib/placeholder-images.ts
+++ src/lib/placeholder-images.ts
@@ -9,19 +9,19 @@ export const PlaceHolderImages: ImagePlaceholder[] = [
   {
     "id": "hero-workspace",
     "description": "Premium graphic design showcase",
-    "imageUrl": "https://res.cloudinary.com/ddqqlfsjp/image/upload/v1773828067/ChatGPT_Image_Mar_18_2026_03_25_18_PM_cqpoxo.png",
+    "imageUrl": "https://res.cloudinary.com/ddqqlfsjp/image/upload/v1774270609/graphic_xzakau.png",
     "imageHint": "creative design"
   },
   {
     "id": "student-work",
     "description": "Graphic design student working on professional project",
-    "imageUrl": "https://res.cloudinary.com/ddqqlfsjp/image/upload/v1773828065/ChatGPT_Image_Mar_18_2026_03_24_37_PM_v7ecvr.png",
+    "imageUrl": "https://res.cloudinary.com/ddqqlfsjp/image/upload/v1774271262/result_0_piixkj.png",
     "imageHint": "designer workspace"
   },
   {
     "id": "branding-mockup",
     "description": "Professional design awards and recognition",
-    "imageUrl": "https://res.cloudinary.com/ddqqlfsjp/image/upload/v1773828065/ChatGPT_Image_Mar_18_2026_03_24_30_PM_phk9xd.png",
+    "imageUrl": "https://res.cloudinary.com/ddqqlfsjp/image/upload/v1774271263/f048846c-e459-472a-9d0c-5f2f57cc01e0_yxodcc.png",
     "imageHint": "design excellence"
   },
   {
