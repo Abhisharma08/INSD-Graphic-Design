3ab704d9384ccd7615c7a22d863f711ff70e77da
Abhisharma08 <aksharma223@gmail.com>
2026-04-06 10:55:36 +0530

    Add preconnect link for Cloudinary and optimize image loading in LandingPage; enhance lead form section styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,25 +30,12 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${inter.variable} ${belleza.variable}`}>
       <head>
+        <link rel="preconnect" href="https://res.cloudinary.com" crossOrigin="anonymous" />
         <Script
           id="google-tag-manager"
           strategy="lazyOnload"
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-T9NSTZP4');`,
-          }}
-        />
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=GTM-K6TRCZN5"
-          strategy="lazyOnload"
-        />
-        <Script
-          id="google-ads-gtag"
-          strategy="lazyOnload"
-          dangerouslySetInnerHTML={{
-            __html: `window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'GTM-K6TRCZN5');`,
           }}
         />
       </head>
